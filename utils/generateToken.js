cf6f29cfd3f1ed430f1aa8ab2c911f8fb91a6df1
Danielaiola-md <112329266+Danielaiola-md@users.noreply.github.com>
2023-12-10 21:56:25 +0100

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -6,7 +6,7 @@ const generateToken = (res, userId) => {
   });
 
   res.cookie("jwt", token, {
-    httpOnly: true
+    httpOnly: true,
     secure: false, // Use secure cookies in production
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
   });
