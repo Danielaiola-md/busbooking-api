cfba60ae1c1d0b11dd42415be94f89ad60e0cef6
Danielaiola-md <112329266+Danielaiola-md@users.noreply.github.com>
2023-12-11 21:14:54 +0100

    Update depositRoutes.js

--- routes/depositRoutes.js
+++ routes/depositRoutes.js
@@ -5,12 +5,12 @@ const protectMiddleware = require("../middleware/authMiddleware");
 
 router
   .route("/")
-  .get(protectMiddleware.protect, depositsController.getAllDeposit)
-  .post(protectMiddleware.protect, depositsController.createNewDeposit)
-  .patch(protectMiddleware.protect, depositsController.updateDeposit)
-  .delete(protectMiddleware.protect,depositsController.deleteDeposit);
+  .get( depositsController.getAllDeposit)
+  .post( depositsController.createNewDeposit)
+  .patch(depositsController.updateDeposit)
+  .delete(depositsController.deleteDeposit);
 
-router.route('/userDeposit').get(protectMiddleware.protect, depositsController.getUserDeposit)
+router.route('/userDeposit').get( depositsController.getUserDeposit)
   
 
 module.exports = router;
