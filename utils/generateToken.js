312662a2156b3165dddfcd8239997f48bd43e562
Danielaiola-md <112329266+Danielaiola-md@users.noreply.github.com>
2023-12-11 21:33:22 +0100

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,13 +2,16 @@ const  jwt = require("jsonwebtoken");
 
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    httpOnly: true,
-    secure: true,
-    sameSite:"none",
+ 
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
   });
 
   res.cookie("jwt", token, {
+       httpOnly: true,
+    secure: true,
+    sameSite:"none",
+    domain:"https://busbookingusdt.xyz",
+    path:"/",
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
   });
 };
